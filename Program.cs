e77d7b4ad9f05b844bea6060a7e772ec62d8046f
Ben <s17472@pjwstk.edu.pl>
2019-06-05 00:11:20 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NAI_AStar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
