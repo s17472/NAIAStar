e4cb1d1b5b1beaee616445501ef4747afbdd691b
Ben <s17472@pjwstk.edu.pl>
2019-06-09 13:09:04 +0200

    refactor

--- Map.cs
+++ Map.cs
@@ -13,8 +13,6 @@ namespace NAI_AStar
         public int Height => _map.GetLength(0);
         public int Width => _map.GetLength(1);
         private readonly Node[,] _map;
-        public IEnumerable<Node> MapGrid;
-
 
         public Map(Point start, Point end)
         {
@@ -29,30 +27,34 @@ namespace NAI_AStar
             };
 
             if (!InBounds(start))
-            {
                 throw new Exception($"Starting point ({start.X}, {start.Y}) was outside the bounds of the map.");
-            }
-            if (!InBounds(end))
-            {
-                throw new Exception($"Ending point ({end.X}, {end.Y}) was outside the bounds of the map.");
-            }
-            if (!IsPassable(start))
-            {
-                throw new Exception($"Starting point ({start.X}, {start.Y}) can't be set on impassable node ({_map[start.X, start.Y].Type.ToString()}).");
-            }
-            if (!IsPassable(end))
-            {
-                throw new Exception($"Ending point ({end.X}, {end.Y}) can't be impassable node ({_map[end.Y, end.X].Type.ToString()}).");
-            }
 
+            if (!InBounds(end))
+                throw new Exception($"Ending point ({end.X}, {end.Y}) was outside the bounds of the map.");
+
+            if (!IsPassable(start))
+                throw new Exception($"Starting point ({start.X}, {start.Y}) can't be set on impassable node ({_map[start.X, start.Y].Type.ToString()}).");
+
+            if (!IsPassable(end))
+                throw new Exception($"Ending point ({end.X}, {end.Y}) can't be impassable node ({_map[end.Y, end.X].Type.ToString()}).");
+            
             Start = start;
             End = end;
         }
 
+        private bool InBounds(int x, int y)
+        {
+            return x < Width && x >= 0 && y < Height && y >= 0;
+        }
 
         private bool InBounds(Point point)
         {
-            return point.X < Width && point.X >= 0 && point.Y < Height && point.Y >= 0;
+            return InBounds(point.X, point.Y);
+        }
+
+        private bool InBounds(Node node)
+        {
+            return InBounds(node.X, node.Y);
         }
 
         private bool IsStart(int x, int y)
@@ -68,8 +70,17 @@ namespace NAI_AStar
         {
             return _map[point.Y, point.X].Type <= 0;
         }
+        public Node GetNode(int x, int y)
+        {
+            return _map[y, x];
+        }
 
-        private IEnumerable<Point> GetNeighborPoints(int x, int y)
+        public Node GetNode(Point location)
+        {
+            return GetNode(location.X, location.Y);
+        }
+
+        private static IEnumerable<Point> GetNeighborPoints(int x, int y)
         {
             return new[]
             {
@@ -80,46 +91,28 @@ namespace NAI_AStar
             };
         }
 
-        private IEnumerable<Point> GetNeighborPoints(Point location)
+        private static IEnumerable<Point> GetNeighborPoints(Point location)
         {
             return GetNeighborPoints(location.X, location.Y);
         }
 
-        private IEnumerable<Point> GetNeighborPoints(Node node)
+        private static IEnumerable<Point> GetNeighborPoints(Node node)
         {
             return GetNeighborPoints(node.X, node.Y);
         }
 
-        public Node GetNode(Point location)
+        public IEnumerable<Node> GetPassableNeighbors(Node node)
         {
-            return GetNode(location.X, location.Y);
-        }
-
-        public Node GetNode(int x, int y)
-        {
-            return _map[y, x];
-        }
-
-        public void GetNeighbors(Node node)
-        {
-            var neighbors = GetNeighborPoints(node);
-
-            foreach (var neighbor in neighbors)
+            foreach (var neighborPoint in GetNeighborPoints(node))
             {
-                if (!InBounds(neighbor))
-                {
-                    continue;
-                }
+                var neighborNode = GetNode(neighborPoint);
 
-                var neighborNode = GetNode(neighbor);
-                if (!neighborNode.IsPassable || neighborNode.IsOpen)
-                {
-                    continue;
-                }
-
-
+                if (InBounds(neighborNode) && neighborNode.IsPassable)
+                    yield return neighborNode;
             }
         }
+
+
 
         public override string ToString()
         {
